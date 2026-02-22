2c8b51f37797b4e32af2173ca95e26abcf73d0b7
MikaSie <mikasie6@gmail.com>
2026-02-22 13:03:57 +0100

    added dataclass to catch.py

--- core/src/ontheline_core/catch.py
+++ core/src/ontheline_core/catch.py
@@ -12,3 +12,11 @@ class catch:
     technique: str | None
     notes: str | None
     gear: dict | None
+
+
+if __name__ == "__main__":
+    print(
+        catch(
+            "10102", datetime.now(), 1.14, 0.14, "perch", "carolina-rig", "Fun catch!"
+        )
+    )
