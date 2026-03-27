a00cda83be34d147ae7806855183e1e5f1719fb2
MikaSie <mikasie6@gmail.com>
2026-03-27 16:59:09 +0100

    fix(config.py/session.py): cahnged db location

--- app/db/session.py
+++ app/db/session.py
@@ -1,6 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
 from app.core.config import config
 
-engine = create_engine(config.db_url, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+config.db_path.parent.mkdir(parents=True, exist_ok=True)
+
+engine = create_engine(
+    config.db_url,
+    connect_args={"check_same_thread": False},
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
